335087354062e48c8d3bcb8307bfb9d54c484264
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-04-21 23:38:05 +0600

    Init

---
+++ EFIntroduction/Migrations/DataContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("name=DataContext")
+        {
+        }
+
+        public DbSet<Record> Records { get; set; }
+    }
+}
