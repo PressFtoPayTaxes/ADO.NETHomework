9c9157df84fe285a1562f37005669fb45c40c006
PressFtoPayTaxes <46023537+PressFtoPayTaxes@users.noreply.github.com>
2019-04-23 22:28:50 +0600

    Init

---
+++ AutoLevelPart2/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLevelPart2
+{
+    class Program
+    {
+        static void ShowUsers()
+        {
+            bool isAdminFiltrationEnabled;
+            while (true)
+            {
+                Console.WriteLine("Включить фильтрацию по админам?(1 - да, 2 - нет)");
+                int answer = int.Parse(Console.ReadLine());
+                switch(answer)
+                {
+                    case 1: isAdminFiltrationEnabled = true; break;
+                    case 2: isAdminFiltrationEnabled = false; break;
+                    default: Console.WriteLine("Такого варианта нет"); continue;
+                }
+                break;
+            }
+
+            UsersHandler handler = new UsersHandler();
+            var usersSet = handler.GetUsers();
+
+            Console.WriteLine("Логин:\t\tПароль:\t\tАдрес:\t\tТелефонный номер:\t\t\tАдмин:");
+            foreach(DataRow row in usersSet.Tables["Users"].Rows)
+            {
+                User user = new User
+                {
+                    Login = row.ItemArray[1].ToString(),
+                    Password = row.ItemArray[2].ToString(),
+                    Address = row.ItemArray[3].ToString(),
+                    PhoneNumber = row.ItemArray[4].ToString(),
+                    IsAdmin = bool.Parse(row.ItemArray[5].ToString())
+                };
+                if (!isAdminFiltrationEnabled)
+                    Console.WriteLine($"{user.Login}\t\t{user.Password}\t\t{user.Address}\t\t{user.PhoneNumber}\t\t\t{user.IsAdmin}");
+                else if (user.IsAdmin)
+                    Console.WriteLine($"{user.Login}\t\t{user.Password}\t\t{user.Address}\t\t{user.PhoneNumber}\t\t\t{user.IsAdmin}");
+            }
+        }
+
+        static void AddUser()
+        {
+            UsersHandler handler = new UsersHandler();
+
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+
+            var users = handler.GetUsers();
+            foreach(DataRow row in users.Tables["Users"].Rows)
+            {
+                if (row.ItemArray[1].ToString() == login)
+                {
+                    Console.WriteLine("Пользователь с таким логином уже существует");
+                    return;
+                }
+            }
+
+            Console.Write("Введите пароль: ");
+            string password = Console.ReadLine();
+            Console.Write("Введите адрес: ");
+            string address = Console.ReadLine();
+            Console.Write("Введите номер телефона: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Является ли он админом? (true, false): ");
+            bool isAdmin = bool.Parse(Console.ReadLine());
+
+            handler.AddUser(login, password, address, phoneNumber, isAdmin);
+        }
+
+        static void RedactUser()
+        {
+            var handler = new UsersHandler();
+            Console.Write("Введите логин редактируемого пользователя: ");
+            string login = Console.ReadLine();
+
+            User user = new User();
+
+            var users = handler.GetUsers();
+            foreach(DataRow row in users.Tables["Users"].Rows)
+            {
+                if (row.ItemArray[1].ToString() == login)
+                {
+                    user = new User
+                    {
+                        Login = row.ItemArray[1].ToString(),
+                        Password = row.ItemArray[2].ToString(),
+                        Address = row.ItemArray[3].ToString(),
+                        PhoneNumber = row.ItemArray[4].ToString(),
+                        IsAdmin = bool.Parse(row.ItemArray[5].ToString())
+                    };
+                    break;
+                }
+            }
+
+            if (user.Login == null)
+            {
+                Console.WriteLine("Такого пользователя нет");
+                return;
+            }
+
+            Console.Write($"Введите новый пароль (старый - {user.Password}): ");
+            string password = Console.ReadLine();
+            Console.Write($"Введите новый адрес (старый - {user.Address}): ");
+            string address = Console.ReadLine();
+            Console.Write($"Введите новый номер телефона (старый - {user.PhoneNumber}): ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write($"Является ли он админом? (сейчас - {user.IsAdmin}): ");
+            bool isAdmin = bool.Parse(Console.ReadLine());
+
+            handler.RedactUser(login, password, address, phoneNumber, isAdmin);
+        }
+
+        static void DeleteUser()
+        {
+            Console.Write("Введите логин удаляемого пользователя: ");
+            string login = Console.ReadLine();
+
+            var handler = new UsersHandler();
+            handler.DeleteUser(login);
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1 - Показать список пользователей\n2 - Добавить пользователя\n3 - Редактировать пользователя\n4 - Удалить пользователя\n5 - Выход");
+                int answer = int.Parse(Console.ReadLine());
+                
+                switch (answer)
+                {
+                    case 1: ShowUsers(); break;
+                    case 2: AddUser(); break;
+                    case 3: RedactUser(); break;
+                    case 4: DeleteUser(); break;
+                    case 5: Environment.Exit(0); break;
+                    default: Console.WriteLine("Такого ответа нет"); break;
+                }
+            }
+
+        }
+    }
+}
---
+++ AutoLevelPart2/UsersHandler.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLevelPart2
+{
+    public class UsersHandler
+    {
+        private readonly string _connectionString;
+        private readonly string _provider;
+
+        public UsersHandler()
+        {
+            _provider = ConfigurationManager.ConnectionStrings["appString"].ProviderName;
+            _connectionString = ConfigurationManager.ConnectionStrings["appString"].ConnectionString;
+        }
+
+        public DataSet GetUsers()
+        {
+            var factory = DbProviderFactories.GetFactory(_provider);
+            var usersSet = new DataSet();
+
+            using (var connection = factory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                connection.ConnectionString = _connectionString;
+                command.CommandText = "select * from Users";
+
+                var dataAdapter = factory.CreateDataAdapter();
+                dataAdapter.SelectCommand = command;
+
+                dataAdapter.Fill(usersSet, "Users");
+            }
+
+            return usersSet;
+        }
+
+        public void AddUser(string login, string password, string address, string phoneNumber, bool isAdmin)
+        {
+            var user = GetUsers();
+            var factory = DbProviderFactories.GetFactory(_provider);
+
+            var dataAdapter = factory.CreateDataAdapter();
+            using (var connection = factory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                connection.ConnectionString = _connectionString;
+                command.CommandText = "select * from Users";
+
+                dataAdapter.SelectCommand = command;
+                var commandBuilder = factory.CreateCommandBuilder();
+                commandBuilder.DataAdapter = dataAdapter;
+
+                var random = new Random();
+                user.Tables["Users"].Rows.Add(random.Next(1, 99999), login, password, address, phoneNumber, isAdmin);
+
+                dataAdapter.Update(user, "Users");
+            }
+        }
+
+        public void RedactUser(string login, string newPassword, string newAddress, string newPhoneNumber, bool newIsAdmin)
+        {
+            var factory = DbProviderFactories.GetFactory(_provider);
+
+            using (var connection = factory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                connection.ConnectionString = _connectionString;
+                command.CommandText = "select * from Users";
+
+                var dataAdapter = factory.CreateDataAdapter();
+                dataAdapter.SelectCommand = command;
+                dataAdapter.SelectCommand.Connection = connection;
+
+                var builder = factory.CreateCommandBuilder();
+                builder.DataAdapter = dataAdapter;
+
+                var users = GetUsers();
+                object neededRow = new object();
+                foreach (DataRow row in users.Tables["Users"].Rows)
+                {
+                    if (row.ItemArray[1].ToString() == login)
+                    {
+                        neededRow = row;
+                        break;
+                    }
+                }
+                var dataRow = neededRow as DataRow;
+                dataRow.BeginEdit();
+                dataRow.ItemArray = new object[] { dataRow.ItemArray[0], dataRow.ItemArray[1], newPassword, newAddress, newPhoneNumber, newIsAdmin };
+                dataRow.EndEdit();
+                dataAdapter.Update(users, "Users");
+            }
+        }
+
+        public void DeleteUser(string login)
+        {
+            var factory = DbProviderFactories.GetFactory(_provider);
+
+            using (var connection = factory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                connection.ConnectionString = _connectionString;
+
+                command.CommandText = "select * from Users";
+
+                var dataAdapter = factory.CreateDataAdapter();
+                dataAdapter.SelectCommand = command;
+
+                var builder = factory.CreateCommandBuilder();
+                builder.DataAdapter = dataAdapter;
+
+                var dataSet = new DataSet();
+                dataAdapter.Fill(dataSet, "Users");
+
+                foreach(DataRow row in dataSet.Tables["Users"].Rows)
+                {
+                    if (row.ItemArray[1].ToString() == login)
+                        row.Delete();
+                }
+
+                dataAdapter.Update(dataSet, "Users");
+            }
+        }
+    }
+}
